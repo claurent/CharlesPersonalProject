7815933cf8f406bf73167c94e96c2d5e13b4ec43
Charles Laurent <charlesrlaurent@gmail.com>
2015-12-26 16:25:53 -0500

    Updated Weather App to call API dynamically

--- app/weatherApp/weatherSearch.js
+++ app/weatherApp/weatherSearch.js
@@ -1,10 +1,76 @@
 'use strict';
 
-myApp.controller('WeatherSearchCtrl', ['$scope', '$log', function($scope, $log) {
+
+myApp.controller('WeatherSearchCtrl', ['$scope', '$location', '$resource', '$log', function($scope, $location, $resource, $log) {
     $scope.cityName = '';
+    $scope.errorMessage = false;
+    $scope.position = {};
+
+    $scope.weatherAPIKey = '6dd0bc9b5374f7f22ade02797695bd8d';
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
+        callback: "JSON_CALLBACK"}, {get: { method: "JSONP"}});
+    $scope.weatherResults = {}
 
 
+    // Search by City Name
     $scope.searchWeather = function() {
-        $log.info($scope.cityName);
+        $log.info('City name: ' + $scope.cityName);
+        $scope.errorMessage = false;
+        $scope.callWeatherAPI({cityName: $scope.cityName});
     }
+
+    // Search by coordinates
+    $scope.getLocation = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+                $log.log(position);
+                $scope.$apply(function() {
+                    $scope.position = position;
+                    $scope.errorMessage = false;
+                });
+                $scope.callWeatherAPI(position);
+            },
+            function(err) {
+                $log.error(err);
+                $scope.$apply(function() {
+                    $scope.errorMessage = err;
+                });
+            })
+
+        }
+        $log.info('location');
+    }
+
+
+
+    $scope.callWeatherAPI = function(input) {
+        var weatherRequestParams = {};
+        if(input.coords){
+            weatherRequestParams = {
+                lat: input.coords.latitude,
+                lon: input.coords.longitude,
+                APPID: $scope.weatherAPIKey
+            }
+        }
+        else if (input.cityName) {
+            weatherRequestParams = {
+                q: input.cityName,
+                APPID: $scope.weatherAPIKey
+            }
+        }
+        else {
+            $log.error('Error');
+            return;
+        }
+
+        $log.log(weatherRequestParams);
+
+        $scope.weatherResults = $scope.weatherAPI.get(weatherRequestParams);
+        $log.log($scope.weatherResults);
+
+
+
+    }
+
+
 }]);
